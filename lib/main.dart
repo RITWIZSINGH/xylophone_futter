f560a4afb32d20b1b855475d4d9c24b51c86e038
Ritwiz <madangopalsingh2803@gmail.com>
2024-03-26 17:55:40 +0530

    started the project

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(XylophoneApp());
+
+// ignore: use_key_in_widget_constructors
+class XylophoneApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Container(),
+        ),
+      ),
+    );
+  }
+}
